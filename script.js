860109060466beab7ad9c72407dac229cb68af7d
vinzeerow <tpv2001@gmail.com>
2023-05-06 10:33:22 +0700

    10:33 06/5/2023

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var url = "http://192.168.43.195:3000/coordinates";
 var map, marker, marker_source, marker_layer, rooms_layer;
+var id_question = [542, 566];
+var questions = [
+    {
+        id: 542,
+        question: "Tại sao ?",
+        answer: "Trả lời",
+        status: true,
+        id_next: 566
+    },
+    {
+        id: 566,
+        question: "Tại sao ?",
+        answer: "Trả lời",
+        status: false,
+        id_next: null
+    }
+]
 
 // function addLayer() {
 //     const source_street = new ol.source.Vector({
@@ -30,33 +47,10 @@ var map, marker, marker_source, marker_layer, rooms_layer;
 //     map.addLayer(layer_street);
 
 
-// map.on('click', function (event) {
-//     // Lấy danh sách các feature ở vị trí click
-//     var features = source_street.getFeaturesAtCoordinate(event.coordinate);
 
-//     // Lặp qua danh sách các feature và log ra thông tin tọa độ
-//     features.forEach(function (feature) {
-//         var geometry = feature.getGeometry();
-//         var coordinates = geometry.getCoordinates();
-//         // console.log(coordinates);
-//         var properties = feature.getProperties();
-
-//         // Log properties của feature gần nhất
-//         // console.log(properties);
-
-//         // var coordinate = ol.proj.fromLonLat([11773742, 1122011]);
-//         // console.log(coordinates[0][0][0]);
-//         var closestFeature = source_street.getClosestFeatureToCoordinate([11773742.68628586, 1122011.89691158]);
-//         if (closestFeature) {
-//             var closestFeatureCoordinates = closestFeature.getGeometry().getCoordinates();
-//             console.log('Closest feature coordinates:', closestFeatureCoordinates);
-//         } else {
-//             console.log('No closest feature found');
-//         }
-//     });
-// });
 
 // }
+
 function initMap() {
     /* Khởi tạo map */
     map = new ol.Map({
@@ -116,11 +110,64 @@ async function getDataRooms() {
     rooms_layer.getSource().addFeatures(features);
     return dataFromAjax;
 }
+// async function getFeatureIds() {
+//     let dataFromAjax = null;
+//     var url = "http://192.168.43.195:3000/";
+//     dataFromAjax = await $.ajax({
+//         type: "GET",
+//         dataType: 'json',
+//         url: ''
+//     });
+//     var features = new ol.format.GeoJSON().readFeatures(JSON.stringify(dataFromAjax));
+//     rooms_layer.getSource().addFeatures(features);
+//     return dataFromAjax;
+// }
 function checkPositionAndFeature(dataFromAjax, pos) {
     const closestFeature = rooms_layer.getSource().getClosestFeatureToCoordinate(pos);
+    // for (var i = 0; i < questions.length; i++) {
+    //     questions[i].status = true;
+    // }
+    var show = true;
+    /* Lúc bắt đầu thì hiện câu đố thứ nhất*/
+    /* Nếu trả lời đúng thì hiển thị id kế tiêp*/
+    /* question.id */
+    /* Người dùng đi đến vị trí question.id thì chổ đó tô màu đỏ*/
+    /* Hiển thị câu đố tiếp theo */
+
+
+    // if (questions[0].status == true) {
+    //     console.log(questions[0].question);
+    //     console.log(questions[0].answer);
+    //     console.log("Trả lời");
+    //     for (var i = 1; i < questions.length; i++) {
+    //         if (questions[0].id_next == questions[i].id) {
+    //             /* tô màu đỏ */
+    //         }
+    //     }
+
+    // }
+    questions.forEach(function (question) {
+
+
+        /* Nếu trả lời đúng thì hiển thị id kế tiêp*/
+        // show = true;
+        /* Nếu trả lời sai thì không hiển thị id kế tiêp*/
+        // show = false;
+
+        if (closestFeature.getProperties().id == question.id) {
+            closestFeature.setStyle(new ol.style.Style({
+                fill: new ol.style.Fill({
+                    color: 'red'
+                })
+            }));
+        }
+
+    });
+
+
     var closestPosition = closestFeature.getGeometry().getClosestPoint(pos);
     console.log("Vị trị của bạn hiện tại gần với feature: ");
-    console.log(closestFeature.getProperties());
+    console.log(closestFeature.getProperties().id);
     var dis = distance(pos[0], pos[1], closestPosition[0], closestPosition[1]);
     console.log('Khoảng cách giữa hai vị trí là: ' + dis + ' mét');
 }
@@ -132,7 +179,8 @@ function distance(x1, y1, x2, y2) {
 function checkRequestAccessLocation(dataFromAjax) {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(function (position) {
-            var pos = ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]);
+            // var pos = ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]);
+            var pos = ol.proj.fromLonLat([105.769588, 10.030725]);
             map.getView().setCenter(pos);
             checkPositionAndFeature(dataFromAjax, pos);
 
@@ -163,10 +211,29 @@ function checkRequestAccessLocation(dataFromAjax) {
         alert('Your browser does not support geolocation.');
     }
 }
+function addQuestionInFeature() {
+    map.on('click', function (event) {
+        // Lấy danh sách các feature ở vị trí click
+        var features = rooms_layer.getSource().getFeaturesAtCoordinate(event.coordinate);
+
+        // Lặp qua danh sách các feature và log ra thông tin tọa độ
+        features.forEach(function (feature) {
+
+            console.log(feature.getProperties().id);
+            // var geometry = feature.getGeometry();
+            // console.log(geometry);
+            // var coordinates = geometry.getCoordinates();
+            // console.log(coordinates);
+
+
+        });
+    });
+}
 $(document).ready(async function () {
     initMap();
     map.addLayer(rooms_layer);
     map.addLayer(marker_layer);
     var dataFromAjax = await getDataRooms();
     checkRequestAccessLocation(dataFromAjax);
+    addQuestionInFeature();
 })
